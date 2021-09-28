2c8c70286cc27db4dbf54df154d69406c63e5c05
UmairKhankhail <59060383+UmairKhankhail@users.noreply.github.com>
2021-09-28 20:47:08 +0500

    A basic function that returns True if the word dog is contained in the input string. Dont worry about edge cases like a punctuation being attached to the word dog, but do account for capitalization.

---
+++ Find a word dog.py
@@ -0,0 +1,10 @@
+def domainGet(name):
+    listo=list(name)
+    a=listo.index('@')
+    empty_list=[]
+    for i in range(len(listo)):
+        if(i>a):
+            empty_list.append(listo[i])
+    combined_list=''.join(empty_list)
+    print(combined_list)
+domainGet('Umair@domain.com')
